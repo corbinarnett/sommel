474a9a6a9140ba66a8c377ff8c14b38f1978b897
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-07 16:07:44 -0800

    add login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,24 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    
+    #find user from db
+    # name="user[email]"
+    @user = User.find_by(email: params[:user][:email])
+
+    #if we find user by email and they have the right password
+    if @user && @user.authenticate(params[:user][:password])
+      #log the user in, remember user from one page to another
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] = "Sorry, please try again."
+      redirect_to login_path
+    end
   end
 
   def destroy
-
+    session.clear
+    redirect_to root_path
   end
 
 end
