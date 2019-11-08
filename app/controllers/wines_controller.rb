6922e62d3d1746da64365bf5485099122022f0e9
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-07 16:55:51 -0800

    add new wine form, not currently working

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -6,4 +6,26 @@ class WinesController < ApplicationController
   def new 
     @wine = Wine.new
   end
+
+  def create 
+    # build compared to new adds the newly created wine to the wines collection
+    @wine = Wine.new(wine_params)
+    if @wine.save
+      redirect_to wine_path(@wine)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @wine = Wine.find_by(id: params[:id])
+  end
+
+
+  private 
+
+  def wine_params
+    # "wine"=>{"name"=>"Pinot Noir", "vintage"=>"2014", "varietal"=>"Pinot "}
+    params.require(:wine).permit(:name, :vintage, :varietal)
+  end
 end
