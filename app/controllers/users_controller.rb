4870c1c736d89e98f6829e3fdef4d5038861f716
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-07 16:00:54 -0800

    add user create/show action, create user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,26 @@ class UsersController < ApplicationController
   end
 
   def create 
-
+    # byebug
+    @user = User.new(user_params)
+    if @user.save
+      #user has been saved we now want to remeber the user when they go to the next page
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+  end
 
+
+  private 
+
+  def user_params
+    # "user"=>{"username"=>"corbintest", "email"=>"corbin@test.com", "password"=>"password"}
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
