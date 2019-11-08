edffccebefb2dc046a08065b57699a306840e642
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-07 16:02:12 -0800

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :tasting_notes
   has_many :wines, through: :tasting_notes
   has_secure_password
+
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
 end
