e49d7526ffe3f449d028280e014d93a9cd7659f7
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-08 22:52:07 -0800

    add working create/new/show actions

--- app/controllers/tasting_notes_controller.rb
+++ app/controllers/tasting_notes_controller.rb
@@ -1,15 +1,24 @@
 class TastingNotesController < ApplicationController
 
+  def show 
+    @user = User.find_by(id: params[:user_id])
+    @tasting_note = @user.tasting_notes.find_by(id: params[:id])
+  end
   def new
-    @tasting_note = current_user.tasting_notes.build
-    @wine = Wine.all
+    @tasting_note = TastingNote.new
+    @wines = Wine.all
   end
 
   def create
-    # "tasting_note"=>{"wine_id"=>"", "rating"=>"", "comment"=>""}
-    # byebug
-  end
+    @user = current_user
+    @tasting_note = current_user.tasting_notes.build(tasting_note_params)
 
+    if @tasting_note.save
+      redirect_to user_tasting_note_path(@user,  @tasting_note)
+    else
+      render :new
+    end
+  end
 
 
 
@@ -18,7 +27,8 @@ class TastingNotesController < ApplicationController
   private
 
   def tasting_note_params
-
+    # "tasting_note"=>{"wine_id"=>"", "rating"=>"", "comment"=>""}
+    params.require(:tasting_note).permit(:wine_id, :rating, :comment)
   end
 
   
