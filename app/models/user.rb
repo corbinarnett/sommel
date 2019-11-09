74e44a3da2ad66db9a1e41dc9d592f5d78b99127
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-09 11:52:03 -0800

    install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :tasting_notes
   has_many :wines, through: :tasting_notes
   has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  
   resources :users, only: [:create, :show] do
     resources :tasting_notes
   end
 
-  resources :producers
+  
   root "sessions#index"
   
   #creating a new session, model object(user) has already been created
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # delete '/logout' => 'sessions#destroy'
 
-  #creating a new model opject so we send the route to the users contoller
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
+  # #creating a new model opject so we send the route to the users contoller
+  # get '/signup' => 'users#new'
+  # post '/signup' => 'users#create'
 
+  resources :producers
   resources :tasting_notes
   resources :wines
   resources :users
