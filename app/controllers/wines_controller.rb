65202e565b070d0c414fbf3b34f8da94d3bb4900
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-09 21:47:54 -0800

    add set wine method to controller

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,4 +1,5 @@
 class WinesController < ApplicationController
+  before_action :set_wine, only: [:show]
   def index 
     @wines = Wine.all
   end
@@ -19,7 +20,7 @@ class WinesController < ApplicationController
   end
 
   def show
-    @wine = Wine.find_by(id: params[:id])
+    # @wine = Wine.find_by(id: params[:id])
   end
 
 private 
@@ -27,5 +28,10 @@ private
   def wine_params
     # "wine"=>{"name"=>"Pinot Noir", "vintage"=>"2014", "varietal"=>"Pinot "}
     params.require(:wine).permit(:name, :vintage, :producer_id)
+  end
+
+  def set_wine
+    @wine = Wine.find_by(id: params[:id])
+    redirect_to wines_path if !@wine
   end
 end
