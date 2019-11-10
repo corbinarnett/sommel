a92848ac97eb8bdc23842c6eb0ac20a632518ddc
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-10 13:14:12 -0800

    add seeds and edit wine index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,18 @@ Producer.create([
   
   ])
 
+
+Wine.create([
+  { name: 'Abbot Claim Pinot Noir', vintage: 2017 , producer_id: 1 }, { name: 'Star Mooring Pinot Noir', vintage: 2017 , producer_id: 1 },
+  { name: 'Le pre du col Pinot Noir', vintage: 2017 , producer_id: 2 }, { name: 'Temperance Hill', vintage: 2017 , producer_id: 2 },
+  { name: 'Pinot Noir Rosé', vintage: 2018 , producer_id: 3 }, { name: 'Gamay Noir', vintage: 2018 , producer_id: 3 },
+  { name: 'Skin Contact Pinot Gris', vintage: 2018 , producer_id: 4 }, { name: 'Blanc De Noir', vintage: 2018 , producer_id: 4 },
+  { name: 'Brut Rosé', vintage: 2015 , producer_id: 5 }, { name: 'Blanc De Blancs', vintage: 2015 , producer_id: 5 },
+  { name: 'Asha Pinot Noir', vintage: 2014 , producer_id: 6 }, { name: 'Jamsheed Pinot Noir', vintage: 2014 , producer_id: 6 },
+  { name: 'Bryan Creek Vineyard Pinot noir', vintage: 2016 , producer_id: 7 }, { name: 'Weber Vineyard Pinot noir', vintage: 2016 , producer_id: 7 },
+  { name: 'Estate Vermentino, Kubli Bench', vintage: 2017 , producer_id: 8 }, { name: 'Vermentino, Orange Wine, Kubli Bench', vintage: 2017 , producer_id: 8 },
+  { name: 'Domaine Danielle Laurent Chardonnay', vintage: 2017 , producer_id: 9 }, { name: 'Hyland Vineyard Pinot Noir', vintage: 2016 , producer_id: 9 },
+  { name: 'Cuvée Pinot Noir', vintage: 2016 , producer_id: 10 }, { name: 'Syrah', vintage: 2015 , producer_id: 10 },
+  
+  ])
+
